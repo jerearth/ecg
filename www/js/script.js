64fb67b03997c538d28b5e5be8fb2267c777dba5
Dominik Dorčák <dominik.dorcak@gmail.com>
2021-11-19 04:09:45 +0100

    priprava na pracu s datami, generator nohodnych dat

--- www/js/script.js
+++ www/js/script.js
@@ -1,50 +1,33 @@
-function getData(n) {
-  var arr = [],
-    i,
-    x,
-    a,
-    b,
-    c,
-    spike;
-  for (
-    i = 0, x = Date.UTC(new Date().getUTCFullYear(), 0, 1) - n * 36e5;
-    i < n;
-    i = i + 1, x = x + 36e5
-  ) {
-    if (i % 100 === 0) {
-      a = 2 * Math.random();
-    }
-    if (i % 1000 === 0) {
-      b = 2 * Math.random();
-    }
-    if (i % 10000 === 0) {
-      c = 2 * Math.random();
-    }
-    if (i % 50000 === 0) {
-      spike = 10;
-    } else {
-      spike = 0;
-    }
-    arr.push([
-      x,
-      2 * Math.sin(i / 100) + a + b + c + spike + Math.random()
-    ]);
-  }
-  return arr;
-}
-var n = 500000,
-  data = getData(n);
 
-
-console.time('line');
 Highcharts.chart('container', {
 
   chart: {
-    zoomType: 'x'
+    zoomType: 'x',
+    animation: false,
+    events: {
+      load: function () {
+        let s =  this.series[0]
+        setInterval(
+          function(){
+            let point = createDummyDataPoint(0)
+            res = (s.data.length<=20) ? s.addPoint(point,true,false) : s.addPoint(point,true,true);
+
+          }
+          ,100)
+        
+      }
+    }
+  
+  },
+
+  plotOptions: {
+    line: {
+        animation: false
+    }
   },
 
   title: {
-    text: 'Highcharts drawing ' + n + ' points'
+    text: 'ECG Chart'
   },
 
   subtitle: {
@@ -58,12 +41,12 @@ Highcharts.chart('container', {
   xAxis: {
     type: 'datetime'
   },
-
   series: [{
-    data: data,
-    lineWidth: 0.5,
-    name: 'Hourly data points'
+    name: 'Random data',
+    data: getECGDataJS(1),
+    marker: {
+      enabled: false,
+    }
   }]
 
 });
-console.timeEnd('line');
